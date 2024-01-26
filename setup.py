51d6561e55ea8cc6e17a4eebc9f4b203e71fa46c
Nick Collier <niwicollier@gmail.com>
2024-01-25 22:17:09 -0600

    added setup.py, eliminated src directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+      name="archaic_project",
+      version="2024.1",
+      author="Nicholas W. Collier",
+      author_email="nwcollier@wisc.edu",
+      packages=find_packages()
+      )
+
