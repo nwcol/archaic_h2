2e0220321897a54b1b4335186a3f38117a03d218
Nick Collier <niwicollier@gmail.com>
2024-10-10 14:56:56 -0500

    tweaked arguments to fit_H2 script

--- archaic/scripts/fit_H2.py
+++ archaic/scripts/fit_H2.py
@@ -14,16 +14,13 @@ def get_args():
     parser.add_argument('-d', '--data_fname', required=True)
     parser.add_argument('-g', '--graph_fname', required=True)
     parser.add_argument('-p', '--options_fname', required=True)
-    parser.add_argument('-o', '--out_prefix', required=True)
+    parser.add_argument('-o', '--out_fname', required=True)
     parser.add_argument('-u', '--u', type=float, default=None)
     parser.add_argument('-H', '--use_H', type=int, default=0)
-    parser.add_argument('--max_iter', nargs='*', type=int, default=[1000])
+    parser.add_argument('--max_iters', nargs='*', type=int, default=[1000])
     parser.add_argument('--method', nargs='*', default=['Powell'])
     parser.add_argument('-v', '--verbosity', type=int, default=10)
     parser.add_argument('--perturb_graph', type=int, default=0)
-    parser.add_argument('--sample_ids', nargs='*', default=[])
-    parser.add_argument('--cluster_id', default='')
-    parser.add_argument('--process_id', default='')
     return parser.parse_args()
 
 
@@ -33,37 +30,25 @@ def main():
     data = H2Spectrum.from_bootstrap_file(
         args.data_fname, graph=demes.load(args.graph_fname)
     )
-    if len(args.sample_ids) > 0:
-        data = data.subset(args.sample_ids)
-
-    if len(args.method) != len(args.max_iter):
-        raise ValueError('')
-    tag = inference.get_tag(args.out_prefix, args.cluster_id, args.process_id)
     if args.perturb_graph:
-        graph_fname = f'{tag}_init.yaml'
+        graph_fname = f'{args.out_fname.replace('.yaml', '')}_init.yaml'
         inference.perturb_graph(
             args.graph_fname, args.options_fname, out_fname=graph_fname
         )
     else:
         graph_fname = args.graph_fname
-
-    for i, method in enumerate(args.method):
-        if len(args.method) > 1:
-            out_fname = f'{tag}_iter{i + 1}.yaml'
-        else:
-            out_fname = f'{tag}.yaml'
-        inference.fit_H2(
-            graph_fname,
-            args.options_fname,
-            data,
-            max_iter=args.max_iter[i],
-            method=method,
-            u=args.u,
-            verbosity=args.verbosity,
-            use_H=args.use_H,
-            out_fname=out_fname
-        )
-        graph_fname = out_fname
+    out_fname = f'{args.out_fname}.yaml'
+    inference.fit_H2(
+        graph_fname,
+        args.options_fname,
+        data,
+        max_iter=args.max_iters,
+        method=args.method,
+        u=args.u,
+        verbosity=args.verbosity,
+        use_H=args.use_H,
+        out_fname=out_fname
+    )
     return 0
 
 
---
+++ models/10-10-24_bases/fit_H2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -m archaic.scripts.fit_H2 $@
